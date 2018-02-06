43025dec2262e2bbd7362c8e685fb85695ae0890
Biyun Wu <beenwu2016@gmail.com>
2018-02-05 22:10:14 -0500

    Adjust code format.

--- designs.js
+++ designs.js
@@ -1,59 +1,58 @@
 $(function(){
-  // Create default canvas
-  makeGrid(10, 10);
+    // Create default canvas
+    makeGrid(10, 10);
 
-  $('input:submit').click(function(evt){
-    // Prevent the browser to refresh automatically
-    evt.preventDefault();
-    // Clear the potentially existing table
-    $('#pixelCanvas').children().remove();
-    // Create and insert table
-    var height = $('#inputHeight').val();
-    var width = $('#inputWidth').val();
-    if(height>100 || width>100){
-      alert("Inuts should be between 0 and 100.");
-    } else {
-      makeGrid(height, width);
+    $('input:submit').click(function(evt){
+        // Prevent the browser to refresh automatically
+        evt.preventDefault();
+        // Clear the potentially existing table
+        $('#pixelCanvas').children().remove();
+        // Create and insert table
+        var height = $('#inputHeight').val();
+        var width = $('#inputWidth').val();
+        if(height>100 || width>100){
+          alert("Inuts should be between 0 and 100.");
+        } else {
+          makeGrid(height, width);
+        }
+    });
+
+    function makeGrid(height, width) {
+        // Create table
+        var table = createTable(height, width);
+        // Inset the created table to DOM
+        $('#pixelCanvas').append(table);
     }
-  });
 
-  function makeGrid(height, width) {
-    // Create table
-    var table = createTable(height, width);
-    // Inset the created table to DOM
-    $('#pixelCanvas').append(table);
-  }
-
-  // Helper method
-  function createTable(column, row){
-    var tds = createTds(row);
-    var tr = "<tr>" + tds + "</tr>";
-    var trs = "";
-    for (var x=0; x<column; x++){
-      trs += tr;
+    // Helper method
+    function createTable(column, row){
+        var tds = createTds(row);
+        var tr = "<tr>" + tds + "</tr>";
+        var trs = "";
+        for (var x=0; x<column; x++){
+          trs += tr;
+        }
+        return trs;
     }
-    return trs;
-  }
 
-  // Helper method
-  function createTds(row){
-    var td = "<td></td>";
-    var tds = "";
-    for (var x=0; x<row; x++){
-      tds += td;
+    // Helper method
+    function createTds(row){
+        var td = "<td></td>";
+        var tds = "";
+        for (var x=0; x<row; x++){
+          tds += td;
+        }
+        return tds;
     }
-    return tds;
-  }
 
-  // Change <td>'s background color if it is clicked
-  $('#pixelCanvas').on('click', 'td', function(){
-    if ($(this).css("background-color") === "rgba(0, 0, 0, 0)"){
-      $(this).css("background-color", $('#colorPicker').val());
-    } else {
-      $(this).css("background-color", "rgba(0, 0, 0, 0)");
-    }
-  });
-
+    // Change <td>'s background color if it is clicked
+    $('#pixelCanvas').on('click', 'td', function(){
+        if ($(this).css("background-color") === "rgba(0, 0, 0, 0)"){
+          $(this).css("background-color", $('#colorPicker').val());
+        } else {
+          $(this).css("background-color", "rgba(0, 0, 0, 0)");
+        }
+    });
 })
 
 

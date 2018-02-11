1cf32de4db27eb98b04349993a7c57b59c4f3ea1
Biyun Wu <beenwu2016@gmail.com>
2018-02-11 14:00:13 -0500

    Optimize algorithm

--- designs.js
+++ designs.js
@@ -1,52 +1,38 @@
 $(function(){
+    // Global variable
+    let canvas = $('table');
+  
     // Create default canvas
     makeGrid(10, 10);
 
-    $('input:submit').click(function(evt){
+    $('form').submit(function(evt){
         // Prevent the browser to refresh automatically
         evt.preventDefault();
         // Clear the potentially existing table
-        $('#pixelCanvas').children().remove();
+        canvas.children().remove();
         // Create and insert table
-        var height = $('#inputHeight').val();
-        var width = $('#inputWidth').val();
-        if(height>100 || width>100){
-          alert("Inuts should be between 0 and 100.");
-        } else {
-          makeGrid(height, width);
-        }
+        const height = $('#inputHeight').val();
+        const width = $('#inputWidth').val();
+        makeGrid(height, width);
     });
 
     function makeGrid(height, width) {
         // Create table
-        var table = createTable(height, width);
+        let table = createTable(height, width);
         // Inset the created table to DOM
-        $('#pixelCanvas').append(table);
+        canvas.append(table);
     }
 
     // Helper method
     function createTable(column, row){
-        var tds = createTds(row);
-        var tr = "<tr>" + tds + "</tr>";
-        var trs = "";
-        for (var x=0; x<column; x++){
-          trs += tr;
-        }
+        let tds, trs;
+        tds = ("<td></td>").repeat(row);
+        trs = ("<tr>" + tds + "</tr>").repeat(column);
         return trs;
     }
 
-    // Helper method
-    function createTds(row){
-        var td = "<td></td>";
-        var tds = "";
-        for (var x=0; x<row; x++){
-          tds += td;
-        }
-        return tds;
-    }
-
     // Change <td>'s background color if it is clicked
-    $('#pixelCanvas').on('click', 'td', function(){
+    canvas.on('click', 'td', function(){
         if ($(this).css("background-color") === "rgba(0, 0, 0, 0)"){
           $(this).css("background-color", $('#colorPicker').val());
         } else {
@@ -54,7 +40,3 @@ $(function(){
         }
     });
 })
-
-
-
-
